6a3558d7dafc66c996cece543d446cffd6d4eaa0
Kathleen Chung <kathleenchung6@gmail.com>
2018-02-18 21:08:22 -0500

    Stub Pasta and Dish classes

---
+++ Pastabilities/Pasta.swift
@@ -0,0 +1,14 @@
+//
+//  Pasta.swift
+//  Pastabilities
+//
+//  Created by Kathleen Chung on 02-18-18.
+//  Copyright © 2018 Kathleen Chung. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Pasta {
+}
